aba439302e6bd3df7a94319cc292fa22b99fb7e2
Christian Newman <cnewman@kent.edu>
2016-07-25 18:38:50 -0400

    Adding tests directory and a small test that does nothing

---
+++ tests/TestCallPolicy.cpp
@@ -0,0 +1,14 @@
+#include <srcSAXEventDispatch.hpp>
+#include <srcSAXHandler.hpp>
+#include <unordered_map>
+#include <unordered_set>
+#include <TypePolicy.hpp>
+#include <srcSAXHandler.hpp>
+#include <srcSAXEventDispatch.hpp>
+#include <FunctionCallPolicy.hpp>
+int main(int argc, char** filename){
+	CallPolicy calldata;
+    srcSAXController control(filename[1]);
+    srcSAXEventDispatch::srcSAXEventDispatcher<CallPolicy> handler {&calldata};
+    control.parse(&handler); //Start parsing
+}
