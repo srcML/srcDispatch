c72d7c9dbe9cc99d93acffcae78cbea66bee71d4
cnewman <cnewman@kent.edu>
2016-11-01 21:51:51 -0400

    Updates to handle line numbers

--- src/policy_classes/ExprStmtPolicy.hpp
+++ src/policy_classes/ExprStmtPolicy.hpp
@@ -2,22 +2,23 @@
 #include <srcSAXHandler.hpp>
 #include <exception>
 #include <set>
+#include <vector>
 class ExprPolicy : public srcSAXEventDispatch::EventListener, public srcSAXEventDispatch::PolicyDispatcher, public srcSAXEventDispatch::PolicyListener {
     public:
         struct ExprData{
-            ExprData(): def{0} {}
+            ExprData() {}
             void clear(){
-                def = -1;
+               def.clear();
+               use.clear();
             }
             std::string nameofidentifier;
-            unsigned int def;
+            std::set<unsigned int> def;
             std::set<unsigned int> use; //could be used multiple times in same expr
         };
-        ExprData data;
+        std::map<std::string, ExprData> dataset;
         ~ExprPolicy(){}
         ExprPolicy(std::initializer_list<srcSAXEventDispatch::PolicyListener *> listeners = {}): srcSAXEventDispatch::PolicyDispatcher(listeners){
-            isAssignment = false;
-            currentLine = 0;
+            seenAssignment = false;
             InitializeEventHandlers();
         }
         void Notify(const PolicyDispatcher * policy, const srcSAXEventDispatch::srcSAXEventContext & ctx) override {} //doesn't use other parsers
@@ -26,16 +27,24 @@ class ExprPolicy : public srcSAXEventDispatch::EventListener, public srcSAXEvent
             return new ExprData(data);
         }
     private:
+        ExprData data;
         std::string currentTypeName, currentExprName, currentModifier, currentSpecifier;
-        unsigned int currentLine;
-        bool isAssignment;
+        std::vector<unsigned int> currentLine;
+        bool seenAssignment;
         void InitializeEventHandlers(){
             using namespace srcSAXEventDispatch;
-            openEventMap[ParserState::op] = [this](srcSAXEventContext& ctx){
+            closeEventMap[ParserState::op] = [this](srcSAXEventContext& ctx){
                 if(ctx.currentToken == "="){
-                    isAssignment = true;
-                    data.def = currentLine; //it's not a use; make it a def
-                    data.use.erase(currentLine); //delete out of use since it's not a use.
+                    auto it = dataset.find(currentExprName);
+                    if(it != dataset.end()){
+                        it->second.use.erase(currentLine.back());
+                        it->second.def.insert(currentLine.back());
+                        currentLine.pop_back();
+                    }else{
+                        std::cerr<<"No such thing as: "<<currentExprName<<std::endl;
+                    }
+                    seenAssignment = true;
+                    std::cerr<<"Back: "<<currentLine.back()<<std::endl;
                 }
             };
             closeEventMap[ParserState::modifier] = [this](srcSAXEventContext& ctx){
@@ -44,18 +53,23 @@ class ExprPolicy : public srcSAXEventDispatch::EventListener, public srcSAXEvent
 
             closeEventMap[ParserState::name] = [this](srcSAXEventContext& ctx){
                 if(ctx.And({ParserState::exprstmt})){
-                    data.use.insert(currentLine); //assume it's a use
-                    data.nameofidentifier = currentExprName;
-                    std::cerr<<data.def<<" "<<data.nameofidentifier<<std::endl;
+                    auto it = dataset.find(currentExprName);
+                    if(it != dataset.end()){
+                        it->second.use.insert(currentLine.back()); //assume it's a use
+                    }else{
+                        data.nameofidentifier = currentExprName;
+                        data.use.insert(currentLine.back());
+                        dataset.insert(std::make_pair(currentExprName, data));
+                    }
                 }
             };
 
             closeEventMap[ParserState::tokenstring] = [this](srcSAXEventContext& ctx){
                 //TODO: possibly, this if-statement is suppressing more than just unmarked whitespace. Investigate.
                 if(!(ctx.currentToken.empty() || ctx.currentToken[0] == ' ')){
+                    currentLine.push_back(ctx.currentLineNumber);
                     if(ctx.And({ParserState::name, ParserState::expr, ParserState::exprstmt}) && ctx.Nor({ParserState::specifier, ParserState::modifier, ParserState::op})){
                         currentExprName = ctx.currentToken;
-                         data.def = ctx.currentLineNumber;
                     }
                     if(ctx.And({ParserState::specifier, ParserState::expr, ParserState::exprstmt})){
                         currentSpecifier = ctx.currentToken;
@@ -67,7 +81,21 @@ class ExprPolicy : public srcSAXEventDispatch::EventListener, public srcSAXEvent
             };
             closeEventMap[ParserState::exprstmt] = [this](srcSAXEventContext& ctx){
                 NotifyAll(ctx);
-                isAssignment = false;
+                for(auto deal : dataset){
+                    std::cerr<<deal.second.nameofidentifier<<std::endl;
+                    std::cerr<<"def { ";
+                    for(auto num : deal.second.def){
+                        std::cerr<<num<<",";
+                    }
+                    std::cerr<<"}"<<std::endl;
+                    std::cerr<<"use { ";
+                    for(auto num : deal.second.use){
+                        std::cerr<<num<<",";
+                    }
+                    std::cerr<<"}"<<std::endl;
+                }
+                seenAssignment = false;
+                currentLine.clear();
                 data.clear();
             };
 
