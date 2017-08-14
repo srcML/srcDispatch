a2e46f465defeab9ce4d6e4e59e4eef93dc6b50a
vzyrianov <vzyriano@kent.edu>
2017-08-13 20:17:32 -0400

    Adding ClassPolicy

---
+++ src/policy_classes/ClassPolicy.hpp
@@ -0,0 +1,161 @@
+/**
+ * @file ClassPolicy.hpp
+ *
+ * @copyright Copyright (C) 2013-2014 SDML (www.srcML.org)
+ *
+ * The srcML Toolkit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The srcML Toolkit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the srcML Toolkit; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef INCLUDED_CLASS_POLICY_HPP
+#define INCLUDED_CLASS_POLICY_HPP
+
+#include <srcSAXEventDispatcher.hpp>
+#include <DeclTypePolicy.hpp>
+#include <DeclDS.hpp>
+#include <FunctionSignaturePolicy.hpp>
+#include <srcSAXHandler.hpp>
+#include <exception>
+#include <stack>
+#include <list>
+
+class ClassPolicy : public srcSAXEventDispatch::EventListener, public srcSAXEventDispatch::PolicyDispatcher, public srcSAXEventDispatch::PolicyListener {
+    public:
+
+    	struct ClassData {
+    		void Clear() {
+      			className = "";
+      			methods.clear();
+      			members.clear();
+      			isStruct = false;
+   			}
+
+   			std::string className;
+   			bool isStruct = false;  //False -> Class; True -> Struct
+   			std::vector<FunctionSignaturePolicy::SignatureData> methods;
+   			std::vector<DeclData> members;
+		};
+
+        ~ClassPolicy() {
+            delete funcSigPolicy;
+            delete declTypePolicy;
+        }
+
+        ClassPolicy(std::initializer_list<srcSAXEventDispatch::PolicyListener *> listeners = {}): srcSAXEventDispatch::PolicyDispatcher(listeners) {
+            funcSigPolicy = new FunctionSignaturePolicy({this});
+            declTypePolicy = new DeclTypePolicy({this});
+
+            InitializeEventHandlers();
+        }
+
+        void Notify(const PolicyDispatcher * policy, const srcSAXEventDispatch::srcSAXEventContext & ctx) override {
+            if (typeid(FunctionSignaturePolicy) == typeid(*policy)) {
+                FunctionSignaturePolicy::SignatureData signatureData = *policy->Data<FunctionSignaturePolicy::SignatureData>();
+                data_stack.top().methods.push_back(signatureData);
+            }
+
+            else if (typeid(DeclTypePolicy) == typeid(*policy)) {
+                if(!(ctx.IsOpen(srcSAXEventDispatch::ParserState::function))) {
+                    DeclData declarationData = *policy->Data<DeclData>();
+                    data_stack.top().members.push_back(declarationData);
+                }
+            }
+        }
+
+    protected:
+
+        void * DataInner() const override {
+            return new std::vector<ClassData>(data);
+        }
+
+    private:
+
+        std::stack<ClassData> data_stack;
+
+        std::vector<ClassData> data;
+
+        FunctionSignaturePolicy *funcSigPolicy;
+        DeclTypePolicy *declTypePolicy;
+
+        bool gotClassName = true;
+
+        void InitializeEventHandlers() {
+            using namespace srcSAXEventDispatch;
+
+            //Classes
+            openEventMap[ParserState::classn] = [this](srcSAXEventContext &ctx) {
+                if(data_stack.empty()) {
+                    ctx.dispatcher->AddListenerDispatch(funcSigPolicy);
+                    ctx.dispatcher->AddListenerDispatch(declTypePolicy);
+                }
+
+                ClassData current; 
+                current.isStruct = false;
+                data_stack.push(current);
+
+                gotClassName = false;
+            };
+
+            closeEventMap[ParserState::classn] = [this](srcSAXEventContext &ctx) {
+                data.push_back(data_stack.top());
+                data_stack.pop();
+
+                if(data_stack.empty()) {
+                    ctx.dispatcher->RemoveListenerDispatch(funcSigPolicy);
+                    ctx.dispatcher->RemoveListenerDispatch(declTypePolicy);
+
+                    NotifyAll(ctx);
+                    data.clear();
+                }
+            };
+
+            //Structs
+            openEventMap[ParserState::structn] = [this](srcSAXEventContext &ctx) {
+                if(data_stack.empty()) {
+                    ctx.dispatcher->AddListenerDispatch(funcSigPolicy);
+                    ctx.dispatcher->AddListenerDispatch(declTypePolicy);
+                }
+
+                ClassData current; 
+                current.isStruct = true;
+                data_stack.push(current);
+
+                gotClassName = false;
+            };
+
+            closeEventMap[ParserState::structn] = [this](srcSAXEventContext &ctx) {
+                data.push_back(data_stack.top());
+                data_stack.pop();
+
+                if(data_stack.empty()) {
+                    ctx.dispatcher->RemoveListenerDispatch(funcSigPolicy);
+                    ctx.dispatcher->RemoveListenerDispatch(declTypePolicy);
+
+                    NotifyAll(ctx);
+                    data.clear();
+                }
+            };
+
+            closeEventMap[ParserState::name] = [this](srcSAXEventContext& ctx){
+                if((ctx.IsOpen(ParserState::structn) || ctx.IsOpen(ParserState::classn)) && !gotClassName) {
+                    data_stack.top().className = ctx.currentToken;
+                    gotClassName = true;
+                }
+            };
+
+
+        }
+};
+
+#endif
---
+++ tests/TestClassPolicy.cpp
@@ -0,0 +1,83 @@
+#include <srcSAXEventDispatcher.hpp>
+#include <srcSAXHandler.hpp>
+#include <srcSAXHandler.hpp>
+#include <ClassPolicy.hpp>
+#include <cassert>
+#include <srcml.h>
+
+std::string StringToSrcML(std::string str){
+	struct srcml_archive* archive;
+	struct srcml_unit* unit;
+	size_t size = 0;
+
+	char * ch;
+
+	archive = srcml_archive_create();
+	srcml_archive_enable_option(archive, SRCML_OPTION_POSITION);
+	srcml_archive_write_open_memory(archive, &ch, &size);
+
+	unit = srcml_unit_create(archive);
+	srcml_unit_set_language(unit, SRCML_LANGUAGE_CXX);
+	srcml_unit_set_filename(unit, "testsrcType.cpp");
+
+	srcml_unit_parse_memory(unit, str.c_str(), str.size());
+	srcml_archive_write_unit(archive, unit);
+
+	srcml_unit_free(unit);
+	srcml_archive_close(archive);
+	srcml_archive_free(archive);
+
+	std::string srcml;
+	srcml.append(ch, size);
+
+	return srcml;
+}
+
+class TestClassPolicy : public srcSAXEventDispatch::PolicyDispatcher, public srcSAXEventDispatch::PolicyListener{
+    public:
+
+        ~TestClassPolicy() { }
+
+        TestClassPolicy(std::initializer_list<srcSAXEventDispatch::PolicyListener *> listeners = {}) : srcSAXEventDispatch::PolicyDispatcher(listeners) { }
+
+        void Notify(const PolicyDispatcher * policy, const srcSAXEventDispatch::srcSAXEventContext & ctx) override {
+            datatotest = *policy->Data<std::vector<ClassPolicy::ClassData>>();
+        }
+
+		void RunTest(){
+			assert(datatotest[0].className == "foo_data");
+			assert(datatotest[0].members[0].nameofidentifier == "z");
+			assert(datatotest[0].members[1].nameofidentifier == "y");
+
+			assert(datatotest[1].className == "foo");
+			assert(datatotest[1].members[0].nameofidentifier == "x");
+			assert(datatotest[1].methods[0].name == "Print");
+		}
+    protected:
+
+        void * DataInner() const override {
+            return (void*)0;
+        }
+
+    private:
+
+        ClassPolicy classpolicy;
+        ClassPolicy::ClassData classdata;
+        std::vector<ClassPolicy::ClassData> datatotest;
+
+};
+
+int main(int argc, char** filename){
+	std::string codestr = "class foo { \n public: \n void Print() { }; \n int x; \n struct foo_data { \n int z; \n int y; \n }; \n };";
+	std::string srcmlstr = StringToSrcML(codestr);
+	
+	try {
+    	TestClassPolicy classdata;
+    	srcSAXController control(srcmlstr);
+    	srcSAXEventDispatch::srcSAXEventDispatcher<ClassPolicy> handler { &classdata };
+    	control.parse(&handler); //Start parsing
+    	classdata.RunTest();
+    } catch(SAXError e) {
+		std::cerr << e.message;
+	}
+}
