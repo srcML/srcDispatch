4d5d57ae817bb417e493ecd69f04eeb60359f3fd
Ender <ender@TheMachine>
2024-08-07 14:39:18 -0400

    Added a NotifyAll Call in Return Policy

--- src/policy_classes/ReturnPolicy.hpp
+++ src/policy_classes/ReturnPolicy.hpp
@@ -17,23 +17,25 @@ class ReturnPolicy : public srcSAXEventDispatch::EventListener, public srcSAXEve
         void Notify(const PolicyDispatcher * policy, const srcSAXEventDispatch::srcSAXEventContext & ctx) override {} // doesn't use other parsers
         void NotifyWrite(const PolicyDispatcher * policy, srcSAXEventDispatch::srcSAXEventContext & ctx) override {} // doesn't use other parsers
 
-        std::unordered_map<std::string, std::vector<unsigned int>>* GetReturnUses() {
+        void ClearCollection() { returnUses.clear(); }
+
+        std::unordered_map<std::string, std::set<unsigned int>>* GetReturnUses() {
             return &returnUses;
         }
     protected:
         void * DataInner() const override {}
         
     private:
-        std::unordered_map<std::string, std::vector<unsigned int>> returnUses; // variablename | line number
+        std::unordered_map<std::string, std::set<unsigned int>> returnUses; // variablename | line number
 
         void InitializeEventHandlers(){
             using namespace srcSAXEventDispatch;
             
             closeEventMap[ParserState::name] = [this](srcSAXEventContext &ctx) {
                 if(ctx.IsOpen({ParserState::returnstmt}) && ctx.IsClosed({ParserState::comment})){
-                    if (returnUses[ctx.currentToken].empty() || ctx.currentLineNumber != returnUses[ctx.currentToken].back()) {
-                        returnUses[ctx.currentToken].push_back(ctx.currentLineNumber);
-                    }
+                    returnUses[ctx.currentToken].insert(ctx.currentLineNumber);
+
+                    NotifyAll(ctx);
                 }
             };
 
