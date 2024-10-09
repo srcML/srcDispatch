d6b7b7c5e4a47fe2357106a4033ccc471ca67e20
Ender <nallahabi2003@gmail.com>
2024-10-08 23:11:44 -0400

    Modified stylistic formatting of ptrs and refs in ClassPolicy

--- src/policy_classes/ClassPolicy.hpp
+++ src/policy_classes/ClassPolicy.hpp
@@ -52,15 +52,16 @@ class ClassPolicy : public srcSAXEventDispatch::EventListener, public srcSAXEven
             delete declTypePolicy;
         }
 
-        ClassPolicy(std::initializer_list<srcSAXEventDispatch::PolicyListener *> listeners = {}): srcSAXEventDispatch::PolicyDispatcher(listeners) {
+        ClassPolicy(std::initializer_list<srcSAXEventDispatch::PolicyListener*> listeners = {}): srcSAXEventDispatch::PolicyDispatcher(listeners) {
             funcSigPolicy = new FunctionSignaturePolicy({this});
             declTypePolicy = new DeclTypePolicy({this});
 
             InitializeEventHandlers();
         }
 
-        void NotifyWrite(const PolicyDispatcher * policy, srcSAXEventDispatch::srcSAXEventContext & ctx) override {} //doesn't use other parsers
-        void Notify(const PolicyDispatcher * policy, const srcSAXEventDispatch::srcSAXEventContext & ctx) override {
+        void NotifyWrite(const PolicyDispatcher* policy, srcSAXEventDispatch::srcSAXEventContext& ctx) override {} //doesn't use other parsers
+
+        void Notify(const PolicyDispatcher* policy, const srcSAXEventDispatch::srcSAXEventContext& ctx) override {
             if (typeid(FunctionSignaturePolicy) == typeid(*policy)) {
                 SignatureData signatureData = *policy->Data<SignatureData>();
                 data_stack.top().methods.push_back(signatureData);
@@ -86,8 +87,8 @@ class ClassPolicy : public srcSAXEventDispatch::EventListener, public srcSAXEven
 
         std::vector<ClassData> data;
 
-        FunctionSignaturePolicy *funcSigPolicy;
-        DeclTypePolicy *declTypePolicy;
+        FunctionSignaturePolicy* funcSigPolicy;
+        DeclTypePolicy*          declTypePolicy;
 
         bool gotClassName = true;
 
