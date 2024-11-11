d14af5d60e05632bbc00a5c702918ee6ced2137f
Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
2024-11-11 10:22:13 +0900

    Put For in hpp to match rest

--- src/policy_classes/ForPolicySingleEvent.hpp
+++ src/policy_classes/ForPolicySingleEvent.hpp
@@ -11,6 +11,7 @@
 #include <srcDispatchUtilities.hpp>
 
 #include <ControlPolicySingleEvent.hpp>
+#include <BlockPolicySingleEvent.hpp>
 
 #include <string>
 #include <vector>
@@ -27,7 +28,10 @@ struct ForData {
     std::shared_ptr<ControlData> control;
     std::shared_ptr<BlockData>   block;
 
-    friend std::ostream& operator<<(std::ostream& out, const ForData& conditionalData);
+    friend std::ostream& operator<<(std::ostream& out, const ForData& conditionalData) {
+        if(!conditionalData.control) return out;
+        return out << *conditionalData.control;
+    }
 };
 
 
@@ -37,24 +41,91 @@ public srcDispatch::PolicyDispatcher,
 public srcDispatch::PolicyListener {
 
 private:
-    ForData  data;
-    std::size_t      conditionalDepth;
-    ControlPolicy  * controlPolicy;
-    BlockPolicy    * blockPolicy;
+    ForData         data;
+    std::size_t     conditionalDepth;
+    ControlPolicy * controlPolicy;
+    BlockPolicy   * blockPolicy;
 
 public:
-    ForPolicy(std::initializer_list<srcDispatch::PolicyListener *> listeners);
-    ~ForPolicy();
+
+    ForPolicy(std::initializer_list<srcDispatch::PolicyListener*> listeners)
+        : srcDispatch::PolicyDispatcher(listeners),
+          data{},
+          conditionalDepth(0),
+          controlPolicy(nullptr),
+          blockPolicy(nullptr) {
+        InitializeForPolicyHandlers();
+    }
+
+    ~ForPolicy() {
+        if (controlPolicy) delete controlPolicy;
+        if (blockPolicy)   delete blockPolicy;
+    }
 
 protected:
-    std::any DataInner() const override;
-    virtual void Notify(const PolicyDispatcher* policy, const srcDispatch::srcSAXEventContext& ctx) override;
-    void NotifyWrite(const PolicyDispatcher* policy, srcDispatch::srcSAXEventContext& ctx) override;
+    std::any DataInner() const { return std::make_shared<ForData>(data); }
+
+    void Notify(const PolicyDispatcher* policy, const srcDispatch::srcSAXEventContext& ctx) {
+        if (typeid(ControlPolicy) == typeid(*policy)) {
+            data.control = policy->Data<ControlData>();
+        } else if (typeid(BlockPolicy) == typeid(*policy)) {
+            data.block = policy->Data<BlockData>();
+        } else {
+            throw srcDispatch::PolicyError(std::string("Unhandled Policy '") + typeid(*policy).name() + '\'');
+        }
+
+        ctx.dispatcher->RemoveListener(nullptr);
+    }
+
+    void NotifyWrite(const PolicyDispatcher* policy, srcDispatch::srcSAXEventContext& ctx) {} //doesn't use other parsers
 
 private:
-    void InitializeForPolicyHandlers();
-    void CollectControlHandlers();
-    void CollectBlockHandlers();
+
+    void InitializeForPolicyHandlers() {
+        using namespace srcDispatch;
+
+        openEventMap[ParserState::forstmt] = [this](srcSAXEventContext& ctx) {                     \
+            if (!conditionalDepth) {
+                conditionalDepth = ctx.depth;
+                data = ForData{};
+                data.startLineNumber = ctx.currentLineNumber;
+                CollectControlHandlers();
+                CollectBlockHandlers();
+            } 
+        };
+
+        // end of policy
+        closeEventMap[ParserState::forstmt] =[this](srcSAXEventContext& ctx) {
+            if (conditionalDepth && conditionalDepth == ctx.depth) {
+                conditionalDepth = 0;
+                data.endLineNumber = ctx.currentLineNumber;
+                NotifyAll(ctx);
+                InitializeForPolicyHandlers();
+            }
+        };
+    }
+
+
+    void CollectControlHandlers() {
+        using namespace srcDispatch;
+        openEventMap[ParserState::control] = [this](srcSAXEventContext& ctx) {
+            if(!conditionalDepth) return;
+            if((conditionalDepth + 1) != ctx.depth) return;
+
+            if (!controlPolicy) controlPolicy = new ControlPolicy{this};
+            ctx.dispatcher->AddListenerDispatch(controlPolicy);  
+        };              
+    }
+
+    void CollectBlockHandlers() {
+        using namespace srcDispatch;
+        openEventMap[ParserState::block] = [this](srcSAXEventContext& ctx) {
+            if(conditionalDepth && (conditionalDepth + 1) == ctx.depth) {
+                if (!blockPolicy) blockPolicy = new BlockPolicy{this};
+                ctx.dispatcher->AddListenerDispatch(blockPolicy);                
+            }
+        };
+    }
 };
 
 #endif
