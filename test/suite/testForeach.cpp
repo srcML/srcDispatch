a99a7eef24577e9f0f167dd4263306a330f4abc3
Ender <nallahabi2003@gmail.com>
2026-08-14 15:44:54 -0400

    Added Java foreach cases
    Might be redundant as they look like ranged-based for in cxx

--- test/suite/testForeach.cpp
+++ test/suite/testForeach.cpp
@@ -287,3 +287,138 @@ BOOST_AUTO_TEST_CASE(block_replace_foreach_cxx) {
 
     BOOST_TEST(runner.GetFunctionInfo().at(0).ToString() == "void foo() {}");
 }
+
+/**
+ * @section Tests Java foreach
+ */
+BOOST_AUTO_TEST_CASE(block_common_foreach_java) {
+    srcDispatch::DispatchRunner runner("Java");
+    runner.RunDispatcher({{
+        "void foo() { for (int n : nums) {} }",
+        "void foo() { for (int n : nums) {} }"
+    }});
+
+    BOOST_TEST(runner.GetDeclStmtInfo().size() == 0);
+    BOOST_TEST(runner.GetFunctionInfo().size() == 1);
+    BOOST_TEST(runner.GetClassInfo().size()    == 0);
+
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block.IsCommon());
+
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->statements.size() == 1);
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->statements.at(0).IsCommon());
+
+    const srcDispatch::ForData& foreachData = *std::any_cast<std::shared_ptr<srcDispatch::ForData>>(
+        runner.GetFunctionInfo().at(0)->block->statements.at(0).GetElement()
+    );
+
+    BOOST_TEST(foreachData.control.IsCommon());
+
+    BOOST_TEST(foreachData.control->init);
+    BOOST_TEST(foreachData.control->init.IsCommon());
+    BOOST_TEST(foreachData.control->init->inits.size() == 1);
+    BOOST_TEST(foreachData.control->condition.ToString() == "");
+    
+    const auto& deltaForeach = runner.GetFunctionInfo().at(0)->block->statements.at(0);
+    BOOST_TEST(deltaForeach.ToString<std::shared_ptr<srcDispatch::ForData>>() == "int n : nums");
+
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->labels.size() == 0);
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->cases.size()  == 0);
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->blocks.size() == 0);
+
+    BOOST_TEST(runner.GetFunctionInfo().at(0).ToString() == "void foo() {}");
+}
+
+BOOST_AUTO_TEST_CASE(block_insert_foreach_java) {
+    srcDispatch::DispatchRunner runner("Java");
+    runner.RunDispatcher({{"void foo() {}", "void foo() { for (int n : nums) {} }"}});
+
+    BOOST_TEST(runner.GetDeclStmtInfo().size() == 0);
+    BOOST_TEST(runner.GetFunctionInfo().size() == 1);
+    BOOST_TEST(runner.GetClassInfo().size()    == 0);
+
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block.IsCommon());
+
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->statements.size() == 1);
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->statements.at(0).IsInsert());
+
+    const srcDispatch::ForData& foreachData = *std::any_cast<std::shared_ptr<srcDispatch::ForData>>(
+        runner.GetFunctionInfo().at(0)->block->statements.at(0).GetElement()
+    );
+
+    BOOST_TEST(foreachData.control.IsInsert());
+
+    BOOST_TEST(foreachData.control->init);
+    BOOST_TEST(foreachData.control->init.IsInsert());
+    BOOST_TEST(foreachData.control->init->inits.size() == 1);
+    BOOST_TEST(foreachData.control->condition.ToString() == "");
+
+    const auto& deltaForeach = runner.GetFunctionInfo().at(0)->block->statements.at(0);
+    BOOST_TEST(deltaForeach.ToString<std::shared_ptr<srcDispatch::ForData>>() == "|int n : nums");
+
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->labels.size() == 0);
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->cases.size()  == 0);
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->blocks.size() == 0);
+
+    BOOST_TEST(runner.GetFunctionInfo().at(0).ToString() == "void foo() {}");
+}
+
+BOOST_AUTO_TEST_CASE(block_delete_foreach_java) {
+    srcDispatch::DispatchRunner runner("Java");
+    runner.RunDispatcher({{"void foo() { for (int n : nums) {} }", "void foo() {}"}});
+
+    BOOST_TEST(runner.GetDeclStmtInfo().size() == 0);
+    BOOST_TEST(runner.GetFunctionInfo().size() == 1);
+    BOOST_TEST(runner.GetClassInfo().size()    == 0);
+
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block.IsCommon());
+
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->statements.size() == 1);
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->statements.at(0).IsDelete());
+
+    const srcDispatch::ForData& foreachData = *std::any_cast<std::shared_ptr<srcDispatch::ForData>>(
+        runner.GetFunctionInfo().at(0)->block->statements.at(0).GetElement()
+    );
+
+    BOOST_TEST(foreachData.control.IsDelete());
+
+    BOOST_TEST(foreachData.control->init);
+    BOOST_TEST(foreachData.control->init.IsDelete());
+    BOOST_TEST(foreachData.control->init->inits.size() == 1);
+    BOOST_TEST(foreachData.control->condition.ToString() == "");
+
+    const auto& deltaForeach = runner.GetFunctionInfo().at(0)->block->statements.at(0);
+    BOOST_TEST(deltaForeach.ToString<std::shared_ptr<srcDispatch::ForData>>() == "int n : nums|");
+
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->labels.size() == 0);
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->cases.size()  == 0);
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->blocks.size() == 0);
+
+    BOOST_TEST(runner.GetFunctionInfo().at(0).ToString() == "void foo() {}");
+}
+
+BOOST_AUTO_TEST_CASE(block_replace_foreach_java) {
+    srcDispatch::DispatchRunner runner("Java");
+    runner.RunDispatcher({{
+        "void foo() { for (int n : nums) {} }",
+        "void foo() { for (int nums : numbers) {} }"
+    }});
+
+    BOOST_TEST(runner.GetDeclStmtInfo().size() == 0);
+    BOOST_TEST(runner.GetFunctionInfo().size() == 1);
+    BOOST_TEST(runner.GetClassInfo().size()    == 0);
+
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block.IsCommon());
+
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->statements.size() == 1);
+
+    const auto& forData = std::any_cast<std::shared_ptr<srcDispatch::ForData>>(
+        runner.GetFunctionInfo().at(0)->block->statements.at(0).GetElement()
+    );
+    forData->control->init.IsChange();
+
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->labels.size() == 0);
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->cases.size()  == 0);
+    BOOST_TEST(runner.GetFunctionInfo().at(0)->block->blocks.size() == 0);
+
+    BOOST_TEST(runner.GetFunctionInfo().at(0).ToString() == "void foo() {}");
+}
